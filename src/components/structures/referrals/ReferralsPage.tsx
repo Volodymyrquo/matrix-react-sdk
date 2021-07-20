35ae0ea4e3a5009baab05c0bfb1c4c163c2b1b2a
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-20 18:23:44 +0300

    resolved issues

--- src/components/structures/referrals/ReferralsPage.tsx
+++ src/components/structures/referrals/ReferralsPage.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import  BlockCalculator  from '../../views/referrals/BlockCalculator';
 
 
 const ReferralsPage: FC = () => {
@@ -7,7 +6,6 @@ const ReferralsPage: FC = () => {
         <div className="sumra-referrals-page">
             <section className="sumra-referrals-page__header">
                 <div className="sumra-referrals-page__inner">
-                    <BlockCalculator />
                 </div>
             </section>
         </div>
