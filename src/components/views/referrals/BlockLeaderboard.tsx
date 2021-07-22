cfdbc0b1461eb33a1153cd5fc2f24421dc44c9f4
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-22 13:50:46 +0300

    added styles for leaderboard

--- src/components/views/referrals/BlockLeaderboard.tsx
+++ src/components/views/referrals/BlockLeaderboard.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
 import arrow from '../../../../res/images/users/arrow.svg';
 import monay from '../../../../res/images/users/monay.svg';
 import st from '../../../../res/images/users/1st.svg';
@@ -7,187 +9,48 @@ import nd from '../../../../res/images/users/nd.svg';
 import rd from '../../../../res/images/users/rd.svg';
 import th from '../../../../res/images/users/th.svg';
 
+const leadersSheet = [
+{ place: "1", reward: st, sup: "st", name: "Jacob Jones", sum: "$252,750" },
+{ place: "2", reward: nd, sup: "nd", name: "Floye Miles", sum: "$210,505" },
+{ place: "3", reward: rd, sup: "rd", name: "Eleanor Pena", sum: "$150,750" },
+{ place: "4", reward: th, sup: "th", name: "Arlene McCoy", sum: "$85,750" },
+{ place: "5", reward: th, sup: "th", name: "Eleanor Pena", sum: "$75,750" },
+{ place: "6", reward: th, sup: "th", name: "Arlene McCoy", sum: "$65,650" },
+{ place: "7", reward: th, sup: "th", name: "Eleanor Pena", sum: "$55,550" },
+{ place: "8", reward: th, sup: "th", name: "Arlene McCoy", sum: "$45,450" },
+{ place: "9", reward: th, sup: "th", name: "Eleanor Pena", sum: "$35,350" },
+{ place: "10", reward: th, sup: "th", name: "Arlene McCoy", sum: "$25,250" },
+];
+
 const BlockLeaderboard: FC = () => {
     return (
-        <div className="global-earnings-main__box-3">
-            <h3 className="global-earnings-main__box-3-title">Leaderboard</h3>
+        <div className="leaderboard-block">
+            <h3 className="leaderboard-block__title">Leaderboard</h3>
             <table>
-                <thead className="global-earnings-main__thead">
-                    <tr className="global-earnings-main__thead">
-                        <th scope="row">
-              1 <sup className="global-earnings-main__sup">st</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={st} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Jacob Jones</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$252,750`}
-                        </td>
-                    </tr>
-                </thead>
                 <tbody>
-                    <tr className="global-earnings-main__bolt">
-                        <th scope="row">
-              2 <sup className="global-earnings-main__sup">nd</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={nd} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Floyd Miles</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$210,505`}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-              3 <sup className="global-earnings-main__sup">rd</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={rd} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Eleanor Pena</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$150,750`}
-                        </td>
-                    </tr>
-                    <tr className="global-earnings-main__bolt">
-                        <th scope="row">
-              4 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Arlene McCoy</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$85,750`}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-              5 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Eleanor Pena</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$150,750`}
-                        </td>
-                    </tr>
-                    <tr className="global-earnings-main__bolt">
-                        <th scope="row">
-              6 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Arlene McCoy</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$85,750`}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-              7 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Eleanor Pena</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$150,750`}
-                        </td>
-                    </tr>
-                    <tr className="global-earnings-main__bolt">
-                        <th scope="row">
-              8 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Arlene McCoy</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$85,750`}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-              9 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Eleanor Pena</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$150,750`}
-                        </td>
-                    </tr>
-                    <tr className="global-earnings-main__bolt global-earnings-main__table-tr">
-                        <th scope="row">
-              10 <sup className="global-earnings-main__sup">th</sup>
-                        </th>
-                        <td className="global-earnings-main__img-td">
-                            <img src={th} className="global-earnings-main__box-3-img" />
-                        </td>
-                        <td className="global-earnings-main__img-td">Arlene McCoy</td>
-                        <td className="global-earnings-main__img-td">
-                            <img
-                                className="global-earnings-main__img-coin"
-                                src={monay}
-                                alt=""
-                            />
-                            {`$85,750`}
-                        </td>
-                    </tr>
+                    {leadersSheet.map(item => (
+                        <tr key={uuidv4()} >
+                            <td >
+                                {item.place} <sup className="leaderboard-block__sup" >{item.sup}</sup>
+                            </td>
+                            <td >
+                                <img src={item.reward} className="leaderboard-block__img-coin" />
+                            </td>
+                            <td >{item.name}</td>
+                            <td >
+                                <img
+                                    className="leaderboard-block__img-coin"
+                                    src={monay}
+                                    alt=""
+                                />
+                                {item.sum}
+                            </td>
+                        </tr>
+
+                    ))}
                 </tbody>
             </table>
-            <Link to="/leaderboard" className="global-earnings-main__box-3-link">
+            <Link to="/leaderboard" className="leaderboard-block__link">
         Go to Leaderboard page
                 <img src={arrow} alt="arrow" />
             </Link>
