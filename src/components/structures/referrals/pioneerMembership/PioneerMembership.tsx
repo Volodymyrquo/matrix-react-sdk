fedc61a9ec08d05d1ec4f0233552818e403689d8
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-29 16:10:08 +0300

    added pineer membership comonent

--- src/components/structures/referrals/pioneerMembership/PioneerMembership.tsx
+++ src/components/structures/referrals/pioneerMembership/PioneerMembership.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import SidebarReferrals from "../SidebarReferrals";
-import { ContactBookProvider } from "../../../../contexts/ContactBook/contextContactBook";
+import PioneerMemberships from 'pioneer-membership';
 
 const PioneerMembership = () => {
     return (
-        <div className="sumra-referrals-main" >
-            <ContactBookProvider>
-
-                <SidebarReferrals />
-                <div style={{ position: 'absolute', top: "200px", right: "300px" }}>
-
-                    <h1 style={{ fontSize: "x-large", fontWeight: "bolder", color: "blueviolet" }}>Hello from PioneerMembership</h1>
-
-                </div>
-            </ContactBookProvider>
-
+        <div className="referrals-page" >
+            <PioneerMemberships />
         </div>
     );
 };
