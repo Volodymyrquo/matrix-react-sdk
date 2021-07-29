e4bb08974d57810891f6b064992852b2c9729628
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-29 18:10:13 +0300

    some changes

--- src/components/structures/referrals/pioneerMembership/PioneerMembership.tsx
+++ src/components/structures/referrals/pioneerMembership/PioneerMembership.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import PioneerMemberships from 'pioneer-membership';
+import { Context } from "../../../../contexts/Routes/context";
 
 const PioneerMembership = () => {
+    const {setSubPage} = useContext(Context)
+
     return (
         <div className="referrals-page" >
-            <PioneerMemberships />
+            <PioneerMemberships setSubPage={setSubPage} />
         </div>
     );
 };
