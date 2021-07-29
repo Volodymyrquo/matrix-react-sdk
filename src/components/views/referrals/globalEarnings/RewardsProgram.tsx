e05e856663efd41711a3a0ac4784b9363d7b1bee
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-29 18:09:19 +0300

    added func setSubPage to pioneer memberships

--- src/components/views/referrals/globalEarnings/RewardsProgram.tsx
+++ src/components/views/referrals/globalEarnings/RewardsProgram.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-const rewardsStyle = {
-    color: "blue", 
-    fontWeight: "bold"
-}
 const RewardsProgram = () => {
+    const rewardsStyle = {
+        color: "blue", 
+        fontWeight: "bold"
+    }
+    
     return (
-        <div className='global-earnings-main'style={{ textAlign: "center", marginTop: "25%" }}>
-            <h1 style={rewardsStyle}>Hello from Rewards Program</h1>
-            <h2 style={rewardsStyle}>We are waiting for design</h2>
+        <div className='global-earnings-main' style={{ textAlign: "center", marginTop: "25%" }}>
+            <h1 style={rewardsStyle}> Hello from Rewards Program </h1>
+            <h2 style={rewardsStyle}> We are waiting for design </h2>
 
         </div>
     );
