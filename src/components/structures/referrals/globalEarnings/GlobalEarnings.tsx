a9c7be776dc883478e0fe990595bbc21434dacbc
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-30 19:04:20 +0300

    done some changes

--- src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
+++ src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
@@ -1,13 +1,12 @@
 import React, { useContext } from "react";
 import GlobalEarningsBtn from "../../../views/referrals/globalEarnings/GlobalEarningsBtn";
 import { Context } from "../../../../contexts/Routes/context";
-import GlobalEarning from 'global-earnings';
 const GlobalEarnings = () => {
     const { table } = useContext(Context);
     return (
         <div className="referrals-page" style={{ padding: "30px" }}>
-            <GlobalEarning />
-
+            <GlobalEarningsBtn />
+            {table}
         </div>
     );
 };
