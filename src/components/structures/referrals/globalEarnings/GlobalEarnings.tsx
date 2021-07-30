2ea94f0c3d1fc25ac7601f6da33dac2054c07e76
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-30 19:22:13 +0300

    added own npm package

--- src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
+++ src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import GlobalEarningsBtn from "../../../views/referrals/globalEarnings/GlobalEarningsBtn";
 import { Context } from "../../../../contexts/Routes/context";
+import Earnings from 'global-earnings';
 const GlobalEarnings = () => {
     const { table } = useContext(Context);
     return (
         <div className="referrals-page" style={{ padding: "30px" }}>
-            <GlobalEarningsBtn />
-            {table}
+            <Earnings />
         </div>
     );
 };
