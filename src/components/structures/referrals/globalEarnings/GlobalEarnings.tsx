d104f38bd49a311e56aabaffc8d26966a3499350
Volodymyr Basok <volodymyrquo@gmail.com>
2021-07-30 18:54:33 +0300

    changed global earnings name component

--- src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
+++ src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import GlobalEarningsBtn from "../../../views/referrals/globalEarnings/GlobalEarningsBtn";
 import { Context } from "../../../../contexts/Routes/context";
+import GlobalEarning from 'global-earnings';
 const GlobalEarnings = () => {
     const { table } = useContext(Context);
     return (
-        <div className="referrals-page" >
-            <GlobalEarningsBtn />
-            {table}
+        <div className="referrals-page" style={{ padding: "30px" }}>
+            <GlobalEarning />
+
         </div>
     );
 };
