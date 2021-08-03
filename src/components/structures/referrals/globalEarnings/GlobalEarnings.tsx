2ab1be2faca35ae7c0210ca8607a9619aae2223c
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-03 10:24:29 +0300

    added path for GlobalEarnings component

--- src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
+++ src/components/structures/referrals/globalEarnings/GlobalEarnings.tsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import GlobalEarningsBtn from "../../../views/referrals/globalEarnings/GlobalEarningsBtn";
 import { Context } from "../../../../contexts/Routes/context";
 import Earnings from 'global-earnings';
+import {useRouteMatch} from "react-router-dom"
 const GlobalEarnings = () => {
     const { table } = useContext(Context);
+    const {path} = useRouteMatch()
     return (
         <div className="referrals-page" style={{ padding: "30px" }}>
-            <Earnings />
+            <Earnings path={path} />
         </div>
     );
 };
